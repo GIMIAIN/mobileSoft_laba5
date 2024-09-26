4b15db83a5342b036bd56c1d52bfa8f912ae0f6b
GIMIAIN <sasha.sasha.korolev2003@gmail.com>
2024-09-26 07:56:00 +0700

    Add calculating Leg by Area

--- app/src/main/java/com/example/mobilesoft/RightAngledTriangle.kt
+++ app/src/main/java/com/example/mobilesoft/RightAngledTriangle.kt
@@ -1,4 +1,33 @@
 package com.example.mobilesoft
 
-class RightAngledTriangle {
+class RightAngledTriangle(private var leg: Double? = null, private var hypotenuse: Double? = null, private var area: Double? = null) {
+
+    fun calculateArea(): Double? {
+        return if (leg != null) {
+            (leg!! * leg!!) / 2
+        } else if (hypotenuse != null) {
+            val legValue = Math.sqrt(hypotenuse!! * hypotenuse!! / 2)
+            (legValue * legValue) / 2
+        } else {
+            area
+        }
+    }
+
+    fun calculateHypotenuse(): Double? {
+        return if (leg != null) {
+            Math.sqrt(2 * leg!! * leg!!)
+        } else {
+            hypotenuse
+        }
+    }
+
+    fun calculateLeg(): Double? {
+        return if (hypotenuse != null) {
+            Math.sqrt(hypotenuse!! * hypotenuse!! / 2)
+        } else if (area != null) {
+            Math.sqrt(2 * area!!)
+        } else {
+            leg
+        }
+    }
 }
